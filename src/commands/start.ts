484e1068e03aef566e9153eaebedc2f5b08bf8bd
Andrew <andrew.publik.mail@gmail.com>
2025-07-31 16:10:56 +0300

    added start data

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -4,14 +4,28 @@ import { logger } from '../instance/loggerInstance'
 
 export const startCommand = async (ctx: Context) => {
    try {
-      const user = await gameDb.Entities.User.findOne({
+      let user = await gameDb.Entities.User.findOne({
          where: { telegramId: ctx.from?.id?.toString() },
       })
       if (!user) {
-         await gameDb.Entities.User.create({
+         user = await gameDb.Entities.User.create({
             name: ctx.from?.first_name || 'No name',
             telegramId: ctx.from?.id?.toString(),
+            energy: 1000,
          }).save()
+
+         const foods = await gameDb.Entities.Food.find()
+         if (!foods.length) {
+            logger.error('No food found in database')
+         } else {
+            const food = foods[Math.floor(Math.random() * foods.length)]
+            await gameDb.Entities.UserInventory.create({
+               userId: user.id,
+               foodId: food.id,
+               quantity: 4,
+               type: gameDb.datatypes.UserInventoryTypeEnum.FOOD,
+            }).save()
+         }
          await ctx.reply('Добро пожаловать, Профессор, в Mutantorium!')
          logger.info(`Created user telegramId: ${ctx.from?.id?.toString()}`)
          return
