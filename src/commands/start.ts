04c3860513b527e1c5a5307be6d5cd408df54113
Andrew <andrew.publik.mail@gmail.com>
2025-11-07 09:58:25 +0300

    chanched start message start app

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,6 +1,7 @@
 import { Context } from 'grammy'
 import * as gameDb from 'game-db'
 import { logger } from '../instance/loggerInstance'
+import config from '../config'
 
 export const startCommand = async (ctx: Context) => {
    try {
@@ -13,12 +14,44 @@ export const startCommand = async (ctx: Context) => {
             telegramId: ctx.from?.id?.toString(),
             energy: 1000,
          }).save()
-         await ctx.reply('Добро пожаловать, Профессор, в Mutantorium!')
+         await ctx.reply(
+            'Добро пожаловать, Профессор, в Mutantorium!\n 🎮 Для старта игры перейдите по ссылке: 🧪',
+            {
+               reply_markup: {
+                  inline_keyboard: [
+                     [
+                        {
+                           text: '🧪 Открыть лабораторию',
+                           web_app: {
+                              url: `${config.urlWebApp}/laboratory`,
+                           },
+                        },
+                     ],
+                  ],
+               },
+            },
+         )
          logger.info(`Created user telegramId: ${ctx.from?.id?.toString()}`)
          return
       }
       logger.info(`Command start user id: ${user.id}`)
-      await ctx.reply('C возвращением, Профессор, в Mutantorium!')
+      await ctx.reply(
+         'C возвращением, Профессор, в Mutantorium!\n 🎮 Для старта игры перейдите по ссылке: 🧪',
+         {
+            reply_markup: {
+               inline_keyboard: [
+                  [
+                     {
+                        text: '🧪 Открыть лабораторию',
+                        web_app: {
+                           url: `${config.urlWebApp}/laboratory`,
+                        },
+                     },
+                  ],
+               ],
+            },
+         },
+      )
    } catch (e) {
       logger.error('Error create user', e)
    }
